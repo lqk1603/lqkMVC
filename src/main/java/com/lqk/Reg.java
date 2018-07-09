5fd74db6c8b7b40ae353d7103179363f8ba0d792
lqk1603 <lqk1603@163.com>
2018-07-09 16:50:32 +0800

    Initial commit

---
+++ src/main/java/com/lqk/Reg.java
@@ -0,0 +1,14 @@
+package com.lqk;
+
+public class Reg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String data="com.lqk.yyy";
+		System.out.println(data.replaceAll("\\.", "/"));
+	}
+
+}
